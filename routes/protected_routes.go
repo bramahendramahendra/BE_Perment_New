39d5f1ea96b23fcbd8b8d969ca7d3d7fd7e63387
Brama Hendra <bramahendramahendra@gmail.com>
2026-03-03 10:17:47 +0700

    penambahan ping dan header user

--- routes/protected_routes.go
+++ routes/protected_routes.go
@@ -9,4 +9,27 @@ import (
 func protectedRoutes(r *gin.RouterGroup) {
 	r.Use(auth_middleware.BearerAuthMiddleware())
 
+	// =============================================
+	// ENDPOINT TESTING — hapus setelah verified ✅
+	// =============================================
+	r.POST("/ping", func(c *gin.Context) {
+		// c.JSON(200, gin.H{
+		// 	"message":      "pong - protected route berhasil",
+		// 	"header_user":  c.GetHeader("User"),  // hasil dari Prioritas 6
+		// 	"header_userq": c.GetHeader("userq"), // pernr | nama dari JWT
+		// 	"header_pernr": c.GetHeader("pernr"), // pernr dari JWT claims
+		// })
+
+		// Ambil semua header yang ada di request
+		allHeaders := make(map[string]string)
+		for key, values := range c.Request.Header {
+			allHeaders[key] = values[0]
+		}
+
+		c.JSON(200, gin.H{
+			"message":     "pong - protected route berhasil",
+			"all_headers": allHeaders,
+		})
+	})
+	// =============================================
 }
